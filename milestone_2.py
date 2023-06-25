3900354f34039c5c7e238d8d20cddcf34901af9e
Saqub <saqub_ali88@hotmail.co.uk>
2023-06-25 16:09:37 +0100

    M2T4_complete

--- milestone_2.py
+++ milestone_2.py
@@ -5,7 +5,10 @@ word_list = ['mangoes', 'apples', 'raspberries', 'pomegranate', 'passion fruit']
 print(word_list)
 
 word = random.choice(word_list)
-print(word)
 
 guess = input("Please enter a single letter as your guess: ")
-print(guess)
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
