d9f8388c45f7e060fe5ca7e77dd2875041f03652
Saqub <saqub_ali88@hotmail.co.uk>
2023-06-25 17:17:44 +0100

    M3T2_complete

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,14 @@
+word = "gerfuffle"
 guess = input("Please enter a single letter as your guess: ")
+letters_in_play = set(word)
 
 while True:
     if len(guess) == 1 and guess.isalpha():
         break
     else:
         guess = (input("Invalid letter. Please, enter a single alphabetical character."))
+
+if guess in letters_in_play:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
