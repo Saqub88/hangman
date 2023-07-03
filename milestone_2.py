05f0bf52aa7985b02f558e9e7434b6fb39202ade
Saqub88 <134635113+Saqub88@users.noreply.github.com>
2023-07-03 19:32:42 +0100

    Update milestone_2.py
    Revised Milestone 2.

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,16 @@
-
-import random
-
+# Task 1
 word_list = ['mangoes', 'apples', 'raspberries', 'pomegranate', 'passion fruit']
 print(word_list)
 
+# Task 2
+import random
 word = random.choice(word_list)
+print(word)
 
+# Task 3
 guess = input("Please enter a single letter as your guess: ")
 
+# Task 4
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
