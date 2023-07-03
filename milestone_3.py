c823853378a6c17c8943361e139b07e46b153cbb
Saqub88 <134635113+Saqub88@users.noreply.github.com>
2023-07-03 19:54:08 +0100

    Update milestone_3.py
    Revised for Milestone 3 Taks 2

--- milestone_3.py
+++ milestone_3.py
@@ -3,20 +3,20 @@ resampled_word = word.lower()
 guess = input("Please enter a single letter as your guess: ")
 letters_in_play = set(resampled_word)
 
-def ask_for_input(current_guess):
-    while True:
-        if len(current_guess) == 1 and current_guess.isalpha():
-            return str(current_guess)
-#            break
-        else:
-            current_guess = (input("Invalid letter. Please, enter a single alphabetical character."))
-
-def check_guess(current_guess):
-    if current_guess.lower() in letters_in_play:
-        print(f"Good guess! {current_guess} is in the word.")
+#def ask_for_input(current_guess):
+while True:
+    if len(guess) == 1 and guess.isalpha():
+#        return str(current_guess)
+        break
     else:
-        print(f"Sorry, {current_guess} is not in the word. Try again.")
+        guess = (input("Invalid letter. Please, enter a single alphabetical character."))
+
+#def check_guess(current_guess):
+if guess.lower() in letters_in_play:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
 
 
-guess = ask_for_input(guess)
-check_guess(guess)
+# guess = ask_for_input(guess)
+# check_guess(guess)
