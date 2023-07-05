0268b515a9171bc4b873973cd4a4ecc0b1a065d9
Saqub88 <134635113+Saqub88@users.noreply.github.com>
2023-07-05 17:42:47 +0100

    Update milestone_3.py
    edited out while function.

--- milestone_3.py
+++ milestone_3.py
@@ -4,12 +4,13 @@ guess = input("Please enter a single letter as your guess: ")
 letters_in_play = set(resampled_word)
 
 #def ask_for_input(current_guess):
-while True:
-    if len(guess) == 1 and guess.isalpha():
+#while True:
+if len(guess) == 1 and guess.isalpha():
+    guess = guess
 #        return str(current_guess)
-        break
-    else:
-        guess = (input("Invalid letter. Please, enter a single alphabetical character."))
+#    break
+else:
+    guess = (input("Invalid letter. Please, enter a single alphabetical character."))
 
 #def check_guess(current_guess):
 if guess.lower() in letters_in_play:
